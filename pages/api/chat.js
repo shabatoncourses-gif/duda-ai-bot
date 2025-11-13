255922a8410b3616f39c33900ebd01cb3e922233
shabatoncourses-gif <shabaton.courses@gmail.com>
2025-11-13 22:03:39 +0200

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -110,7 +110,7 @@ async function loadIndexes() {
 }
 
 // ===== API HANDLER =====
-export default async function handler(req, res) {
+export default async function chathandler(req, res) {
   res.setHeader("Content-Type", "application/json; charset=utf-8");
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
